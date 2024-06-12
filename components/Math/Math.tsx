0da421bcbab6653e37119d80a2846df4f2e60146
Bruno <bruno.if@hotmail.com>
2024-06-12 11:48:00 -0300

    finished math component

---
+++ components/Math/Math.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MathJaxContext, MathJax } from 'better-react-mathjax';
+import { MathJaxContextProps } from 'better-react-mathjax';
+import { MathJax3Config } from 'better-react-mathjax';
+
+export type MathProps = {
+  MathExpression: string 
+  
+} & MathJaxContextProps;
+
+const Math = ({ MathExpression, ...contextProps }: MathProps) => {
+  const config: MathJax3Config = {
+    loader: { load: ['input/asciimath', 'output/svg'] },
+};
+  
+  return (
+    <MathJaxContext config={config}>
+        <MathJax>{`${MathExpression}`}</MathJax>
+    </MathJaxContext>
+)
+}
+
+export default Math;
