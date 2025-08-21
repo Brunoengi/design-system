eac491243b3c5c2bf4abf13bd63fba1745946994
Bruno Santos <bruno.if@hotmail.com>
2025-08-20 22:00:37 -0300

    Button modify

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,19 +1,42 @@
-import { VariantProps } from "class-variance-authority"
-import { ButtonVariant } from "./Button.style"
+import type { VariantProps } from "class-variance-authority";
+import React, { forwardRef } from "react";
+import { ButtonVariant } from "./Button.style";
 
-export type ButtonProps = VariantProps<typeof ButtonVariant> 
-  & React.ButtonHTMLAttributes<HTMLButtonElement>
-
-
-const Button = ({ children, className, disabled = false, ...rest }: ButtonProps) => {
-
-  return <button
-    className={`${ButtonVariant(rest)} ${className}`}
-    disabled={disabled}
-    {...rest}
-  >
-    {children}
-  </button>
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof ButtonVariant> {
+  children: React.ReactNode;
 }
 
-export default Button
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      className,
+      disabled = false,
+      // 1. Desestruturamos as props de variante para removê-las do `...rest`
+      backgroundColor,
+      borderRadius,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <button
+        ref={ref}
+        // 2. Passamos as props de variante e a className para o CVA
+        className={ButtonVariant({ backgroundColor, borderRadius, className })}
+        disabled={disabled}
+        // 3. O `...rest` agora contém apenas atributos HTML válidos
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
+
