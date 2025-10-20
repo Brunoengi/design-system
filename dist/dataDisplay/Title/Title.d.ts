f605584401596906c7633dfc4207cace99f95551
Bruno Santos <bruno.if@hotmail.com>
2025-10-19 21:53:17 -0300

    modify version

--- dist/dataDisplay/Title/Title.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypographyProps } from "../Typography/Typography";
-export type TitleProps = TypographyProps;
-declare function Title({ children, component, className, ...rest }: TitleProps): import("react/jsx-runtime").JSX.Element;
-export default Title;
